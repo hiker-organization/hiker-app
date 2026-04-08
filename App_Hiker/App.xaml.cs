cdf1b8b6dc10136c44d3b7f802014ee1596a6173
PABLO VALENTIN <pablo.valentin@fatec.sp.gov.br>
2026-04-08 00:08:47 -0300

    :sparkles: feat(pablo/develop): Create themes colors files.

--- App_Hiker/App.xaml.cs
+++ App_Hiker/App.xaml.cs
@@ -7,11 +7,68 @@ namespace App_Hiker
         public App()
         {
             InitializeComponent();
+
+            ApplyRequestedThemeColors();
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AppShell());
+            Window window = new Window(new AppShell());
+
+            window.Height = 600;
+            window.Width = 350;
+
+            return window;
+        }
+
+        private void SetRequestedTheme(AppTheme requested_theme)
+        {
+            Application? current_app = (Application?) App.Current;
+
+            if (current_app != null)
+            {
+                ICollection<ResourceDictionary> app_dictionaries = current_app.Resources.MergedDictionaries;
+
+                ResourceDictionary? current_app_theme = app_dictionaries.FirstOrDefault(dictionary =>
+                {
+                    return dictionary is Resources.Styles.Dark.Colors || dictionary is Resources.Styles.Light.Colors;
+                });
+
+                if (current_app_theme != null)
+                {
+                    app_dictionaries.Remove(current_app_theme);
+                }
+
+                switch (requested_theme)
+                {
+                    case AppTheme.Dark:
+                        app_dictionaries.Add(new Resources.Styles.Dark.Colors());
+                    break;
+
+                    default:
+                        app_dictionaries.Add(new Resources.Styles.Light.Colors());
+                    break;
+                }
+            }
+        }
+
+        private void ApplyRequestedThemeColors()
+        {
+            Application? current_app = (Application?)App.Current;
+
+            if (current_app != null)
+            {
+                // Aplicação inicial.
+
+                SetRequestedTheme(current_app.RequestedTheme);
+
+                // Evento de troca de tema durante a execução do aplicativo.
+
+                current_app.RequestedThemeChanged += (sender, e) =>
+                {
+                    SetRequestedTheme(e.RequestedTheme);
+                };
+            }
         }
     }
 }
---
+++ App_Hiker/Resources/Styles/Dark/Colors.xaml.cs
@@ -0,0 +1,9 @@
+namespace App_Hiker.Resources.Styles.Dark;
+
+public partial class Colors : ResourceDictionary
+{
+	public Colors()
+	{
+		InitializeComponent();
+	}
+}
