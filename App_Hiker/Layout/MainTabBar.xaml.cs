9ebdfea9a8a1ddf16101f593701805ba784803bd
PABLO VALENTIN <pablo.valentin@fatec.sp.gov.br>
2026-05-07 23:51:42 -0300

    :sparkles: feat(pablo/develop): Develop the user profile page structure.

--- App_Hiker/Layout/MainTabBar.xaml.cs
+++ App_Hiker/Layout/MainTabBar.xaml.cs
@@ -4,40 +4,46 @@ namespace App_Hiker.Layout;
 
 public partial class MainTabBar : ContentPage
 {
+	private Application? current_app = (Application?)App.Current;
+
 	private enum InternalTabs
 	{
 		Home,
-		New,
+		NewPost,
 		Profile
 	};
 
-	private int current_tab_index = 0;
+	private int current_main_tab_index = 0;
 
 	public MainTabBar()
 	{
 		InitializeComponent();
 
-		LoadTab();
+
+        LoadTab();
 	}
 
 	private async void ApplyTabsStyles()
 	{
 		try
 		{
-			foreach (IView tab in grid_tabs.Children)
+			if (current_app != null)
 			{
-				if (tab is Button button)
-				{
-					if (Grid.GetColumn(button) == current_tab_index)
-					{
-						button.TextColor = (Color)Application.Current.Resources["Primary"];
-					}
-					else
-					{
-						button.TextColor = (Color)Application.Current.Resources["BaseContent"];
-					}
-				}
-			}
+                foreach (IView tab in grid_main_tabs.Children)
+                {
+                    if (tab is Button button)
+                    {
+                        if (Grid.GetColumn(button) == current_main_tab_index)
+                        {
+                            button.TextColor = (Color)current_app.Resources["Primary"];
+                        }
+                        else
+                        {
+                            button.TextColor = (Color)current_app.Resources["BaseContent"];
+                        }
+                    }
+                }
+            }
 		}
 		catch (Exception ex)
 		{
@@ -49,7 +55,7 @@ public partial class MainTabBar : ContentPage
 	{
 		try
 		{
-            InternalTabs tab_option = (InternalTabs)this.current_tab_index;
+            InternalTabs tab_option = (InternalTabs)this.current_main_tab_index;
 
             switch (tab_option)
             {
@@ -57,7 +63,7 @@ public partial class MainTabBar : ContentPage
                     ctview_page.Content = new Label() { Text = "Aba 01" };
                 break;
 
-                case InternalTabs.New:
+                case InternalTabs.NewPost:
                     ctview_page.Content = new Label() { Text = "Aba 02" };
                     break;
 
@@ -80,7 +86,7 @@ public partial class MainTabBar : ContentPage
 		{
 			Button selected_tab = (Button)sender;
 
-			this.current_tab_index = Grid.GetColumn(selected_tab);
+			this.current_main_tab_index = Grid.GetColumn(selected_tab);
 
 			LoadTab();
 
--- App_Hiker/View/User/Profile.xaml.cs
+++ App_Hiker/View/User/Profile.xaml.cs
@@ -2,8 +2,95 @@ namespace App_Hiker.View.User;
 
 public partial class Profile : ContentView
 {
+    private Application? current_app = (Application?)App.Current;
+
+    private enum InternalTabs
+    {
+        Posts,
+        TimeLine,
+        Favorites
+    };
+
+    private int current_profile_tab_index = 0;
+
 	public Profile()
 	{
 		InitializeComponent();
+
+        LoadTab();
 	}
+
+    private async void ApplyTabsStyles()
+    {
+        try
+        {
+            if (current_app != null)
+            {
+                foreach (IView tab in grid_profile_tabs.Children)
+                {
+                    if (tab is Button button)
+                    {
+                        if (Grid.GetColumn(button) == current_profile_tab_index)
+                        {
+                            button.TextColor = (Color)current_app.Resources["Primary"];
+                        }
+                        else
+                        {
+                            button.TextColor = (Color)current_app.Resources["BaseContent"];
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            App.ShowInDebugConsole(ex.Message); // Temporário.
+        }
+    }
+
+    private async void LoadTab()
+    {
+        try
+        {
+            InternalTabs tab_option = (InternalTabs)this.current_profile_tab_index;
+
+            switch (tab_option)
+            {
+                case InternalTabs.Posts:
+                    ctv_profile_current_tab.Content = new Label() { Text = "Aba 01 (Perfil)" };
+                break;
+
+                case InternalTabs.TimeLine:
+                    ctv_profile_current_tab.Content = new Label() { Text = "Aba 02 (Perfil)" };
+                break;
+
+                case InternalTabs.Favorites:
+                    ctv_profile_current_tab.Content = new Label() { Text = "Aba 03 (Perfil)" };
+                break;
+            }
+
+            ApplyTabsStyles();
+        }
+        catch (Exception ex)
+        {
+            App.ShowInDebugConsole(ex.Message); // Temporário.
+        }
+    }
+
+    private async void tab_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Button selected_tab = (Button)sender;
+
+            this.current_profile_tab_index = Grid.GetColumn(selected_tab);
+
+            LoadTab();
+
+        }
+        catch (Exception ex)
+        {
+            App.ShowInDebugConsole(ex.Message); // Temporário.
+        }
+    }
 }
